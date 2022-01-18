ccac50e913ac36aa627f185618f07df183a3aacd
area363 <kidonseo@gmail.com>
2022-01-18 22:01:28 +0900

    add new fields

--- NineChronicles.DataProvider/GraphQL/Types/CraftRankingType.cs
+++ NineChronicles.DataProvider/GraphQL/Types/CraftRankingType.cs
@@ -11,6 +11,11 @@
             Field(x => x.AgentAddress);
             Field(x => x.BlockIndex);
             Field(x => x.CraftCount);
+            Field(x => x.Name);
+            Field(x => x.AvatarLevel, nullable: true);
+            Field(x => x.TitleId, nullable: true);
+            Field(x => x.ArmorId, nullable: true);
+            Field(x => x.Cp, nullable: true);
             Field(x => x.Ranking);
 
             Name = "CraftRanking";
