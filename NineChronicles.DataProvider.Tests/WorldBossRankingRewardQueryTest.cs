8ebf0405fa661b3faf5500f732bc9cf0011ac196
Yang Chun Ung <qooraven@gmail.com>
2022-10-11 15:39:12 +0900

    Introduce WorldBossRankingRewards query

--- NineChronicles.DataProvider.Tests/WorldBossRankingRewardQueryTest.cs
+++ NineChronicles.DataProvider.Tests/WorldBossRankingRewardQueryTest.cs
@@ -58,24 +58,7 @@ public class WorldBossRankingRewardQueryTest : TestBase
 
         Assert.NotNull(queryAddress);
 
-        var block = new BlockModel
-        {
-            Index = blockIndex,
-            Hash = "4582250d0da33b06779a8475d283d5dd210c683b9b999d74d03fac4f58fa6bce",
-            Miner = "47d082a115c63e7b58b1532d20e631538eafadde",
-            Difficulty = 0L,
-            Nonce = "dff109a0abf1762673ed",
-            PreviousHash = "asd",
-            ProtocolVersion = 1,
-            PublicKey = ByteUtil.Hex(new PrivateKey().PublicKey.ToImmutableArray(false)),
-            StateRootHash = "ce667fcd0b69076d9ff7e7755daa2f35cb0488e4c47978468dfbd6b88fca8a90",
-            TotalDifficulty = 0L,
-            TxCount = 1,
-            TxHash = "fd47c10ffbee8ff2da8fa08cec3072de06a72f73693f5d3399b093b0877fa954",
-            TimeStamp = DateTimeOffset.UtcNow
-        };
-        Context.Blocks.Add(block);
-        await Context.SaveChangesAsync();
+        await SaveBlock(blockIndex);
 
 
         var query = $@"query {{
@@ -113,6 +96,75 @@ public class WorldBossRankingRewardQueryTest : TestBase
         }
     }
 
+    [Theory]
+    [InlineData(11L, true, 100, 2, 101, 2)]
+    [InlineData(11L, true, 0, 2, 1, 2)]
+    [InlineData(11L, true, 100, 100, 101, 100)]
+    [InlineData(11L, true, 0, 50, 1, 50)]
+    [InlineData(11L, false, 100, 2, 100, 2)]
+    public async Task WorldBossRankingRewards(long blockIndex, bool canReceive, int offset, int limit, int expectedRank, int expectedCount)
+    {
+        if (canReceive)
+        {
+            _csv = @"id,boss_id,started_block_index,ended_block_index,fee,ticket_price,additional_ticket_price,max_purchase_count
+1,900001,0,10,300,200,100,10";
+        }
+        for (int i = 0; i < 200; i++)
+        {
+            var avatarAddress = new PrivateKey().ToAddress();
+            var model = new RaiderModel(
+                1,
+                i.ToString(),
+                200 -i,
+                200 - i,
+                i + 2,
+                GameConfig.DefaultAvatarArmorId,
+                i,
+                avatarAddress.ToHex(),
+                0
+            );
+            Context.Raiders.Add(model);
+        }
+
+        await SaveBlock(blockIndex);
+
+
+        var query = $@"query {{
+        worldBossRankingRewards(raidId: 1, offset: {offset}, limit: {limit}) {{
+            ranking
+            rewards {{
+                quantity
+                currency {{
+                    minters
+                    ticker
+                    decimalPlaces
+                }}
+            }}
+        }}
+    }}";
+        var result = await ExecuteAsync(query);
+        if (canReceive)
+        {
+            var queryData = (object[])((Dictionary<string, object>) ((ExecutionNode) result.Data).ToValue())["worldBossRankingRewards"];
+            Assert.Equal(expectedCount, queryData.Length);
+            var dictionary = (Dictionary<string, object>)queryData.First();
+            Assert.Equal(expectedRank, dictionary["ranking"]);
+            var models = (object[])dictionary["rewards"];
+            Assert.True(models.Any());
+            foreach (var model in models)
+            {
+                var rewardInfo = Assert.IsType<Dictionary<string, object>>(model);
+                var quantity = (string)rewardInfo["quantity"];
+                var rawCurrency = (Dictionary<string, object>)rewardInfo["currency"];
+                var currency = new Currency(ticker: (string) rawCurrency["ticker"], decimalPlaces: (byte) rawCurrency["decimalPlaces"], minters: (IImmutableSet<Address>?) rawCurrency["minters"]);
+                FungibleAssetValue.Parse(currency, quantity);
+            }
+        }
+        else
+        {
+            Assert.Single(result.Errors!);
+        }
+    }
     protected override IValue? GetStateMock(Address address)
     {
         if (address.Equals(Addresses.GetSheetAddress<WorldBossListSheet>()))
@@ -152,5 +204,28 @@ public class WorldBossRankingRewardQueryTest : TestBase
     protected override FungibleAssetValue GetBalanceMock(Address address, Currency currency)
     {
         return FungibleAssetValue.FromRawValue(currency, 0);
+    }
+
+    private async Task SaveBlock(long blockIndex)
+    {
+
+        var block = new BlockModel
+        {
+            Index = blockIndex,
+            Hash = "4582250d0da33b06779a8475d283d5dd210c683b9b999d74d03fac4f58fa6bce",
+            Miner = "47d082a115c63e7b58b1532d20e631538eafadde",
+            Difficulty = 0L,
+            Nonce = "dff109a0abf1762673ed",
+            PreviousHash = "asd",
+            ProtocolVersion = 1,
+            PublicKey = ByteUtil.Hex(new PrivateKey().PublicKey.ToImmutableArray(false)),
+            StateRootHash = "ce667fcd0b69076d9ff7e7755daa2f35cb0488e4c47978468dfbd6b88fca8a90",
+            TotalDifficulty = 0L,
+            TxCount = 1,
+            TxHash = "fd47c10ffbee8ff2da8fa08cec3072de06a72f73693f5d3399b093b0877fa954",
+            TimeStamp = DateTimeOffset.UtcNow
+        };
+        Context.Blocks.Add(block);
+        await Context.SaveChangesAsync();        
     }
 }
