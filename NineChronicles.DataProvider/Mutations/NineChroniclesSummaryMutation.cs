f8977dfa9d7165387ef53f0f6388421120645ac0
hskim881028 <andykim881028@gmail.com>
2022-10-11 18:46:49 +0900

    Send `update raider mutation` result message using slack web hook

--- NineChronicles.DataProvider/Mutations/NineChroniclesSummaryMutation.cs
+++ NineChronicles.DataProvider/Mutations/NineChroniclesSummaryMutation.cs
@@ -2,11 +2,13 @@
 {
     using System.Collections.Generic;
     using System.Linq;
+    using System.Net.Http;
+    using System.Text;
+    using System.Threading.Tasks;
     using Bencodex.Types;
     using GraphQL;
     using GraphQL.Types;
     using Libplanet;
-    using Libplanet.Explorer.GraphTypes;
     using Nekoyume;
     using Nekoyume.Model.State;
     using Nekoyume.TableData;
@@ -18,6 +20,8 @@
 
     internal class NineChroniclesSummaryMutation : ObjectGraphType
     {
+        private readonly string _webHookUrl = $"https://hooks.slack.com/services/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";
+
         public NineChroniclesSummaryMutation(MySqlStore store, StandaloneContext standaloneContext, StateContext stateContext)
         {
             Field<BooleanGraphType>(
@@ -56,10 +60,25 @@
                     var updateList = new List<RaiderModel>();
                     var storeRaiders = store.GetRaiderList();
                     UpdateRaider(stateContext, avatarAddresses, raidId, storeRaiders, updateList);
-
                     store.StoreRaiderList(updateList);
-                    return true;
+                    var task = SendMessageAsync(updateList);
+                    return task.Result;
                 });
+        }
+
+        private async Task<bool> SendMessageAsync(List<RaiderModel> raiderList)
+        {
+            var sb = new StringBuilder($"[UPDATE RAIDERS RESULT] user count : {raiderList.Count}\n");
+            foreach (var model in raiderList)
+            {
+                sb.Append($"[{model.Address}] total score : {model.TotalScore}\n");
+            }
+
+            var json = System.Text.Json.JsonSerializer.Serialize(new { text = sb.ToString() });
+            var content = new FormUrlEncodedContent(new Dictionary<string, string> { { "payload", json }, });
+            using var httpClient = new HttpClient();
+            var response = await httpClient.PostAsync(_webHookUrl, content);
+            return response.StatusCode == System.Net.HttpStatusCode.OK;
         }
 
         private void UpdateRaider(StateContext stateContext, List<Address> avatarAddresses, int raidId, List<RaiderModel> storeRaiders, List<RaiderModel> updateList)
