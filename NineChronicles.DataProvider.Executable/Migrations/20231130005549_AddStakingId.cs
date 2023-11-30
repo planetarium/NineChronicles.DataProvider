25824b274d740d22e499c7e811e16488add581f8
area363 <kidonseo@gmail.com>
2023-11-30 10:07:27 +0900

    fix stake logging

---
+++ NineChronicles.DataProvider.Executable/Migrations/20231130005549_AddStakingId.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NineChronicles.DataProvider.Executable.Migrations
+{
+    public partial class AddStakingId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Id",
+                table: "Stakings",
+                type: "varchar(255)",
+                nullable: false,
+                defaultValue: string.Empty)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Stakings",
+                table: "Stakings",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Stakings",
+                table: "Stakings");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Stakings");
+        }
+    }
+}
