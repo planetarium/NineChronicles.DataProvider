d3a56289f7cfa4c8d0ff85f273af3789020aa434
Yang Chun Ung <qooraven@gmail.com>
2024-02-14 22:46:14 +0900

    Introduce ActivateCollection model

--- NineChronicles.DataProvider.Executable/Migrations/20240214134559_ActivateCollection.Designer.cs
+++ NineChronicles.DataProvider.Executable/Migrations/20240214134559_ActivateCollection.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NineChronicles.DataProvider.Store;
 
@@ -10,9 +11,10 @@ using NineChronicles.DataProvider.Store;
 namespace NineChronicles.DataProvider.Executable.Migrations
 {
     [DbContext(typeof(NineChroniclesContext))]
-    partial class NineChroniclesContextModelSnapshot : ModelSnapshot
+    [Migration("20240214134559_ActivateCollection")]
+    partial class ActivateCollection
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
