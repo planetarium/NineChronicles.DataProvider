c513eeaf5b30e299698635dd744f058fe1cfdbd9
hyeon <hyeon@planetariumhq.com>
2024-03-18 22:29:50 +0900

    Add explicit PK

--- NineChronicles.DataProvider/Store/Models/CollectionOptionModel.cs
+++ NineChronicles.DataProvider/Store/Models/CollectionOptionModel.cs
@@ -1,7 +1,12 @@
 namespace NineChronicles.DataProvider.Store.Models
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class CollectionOptionModel
     {
+        [Key]
+        public int Id { get; set; }
+
         public string StatType { get; set; } = null!;
 
         public string OperationType { get; set; } = null!;
