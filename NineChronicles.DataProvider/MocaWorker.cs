66a553b00e0c53151fe380e92533b75d6558a038
Yang Chun Ung <qooraven@gmail.com>
2024-05-30 16:58:23 +0900

    Fix migration

--- NineChronicles.DataProvider/MocaWorker.cs
+++ NineChronicles.DataProvider/MocaWorker.cs
@@ -27,137 +27,84 @@ namespace NineChronicles.DataProvider
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-#pragma warning disable S125
-            // var offset = 0;
-#pragma warning restore S125
-            // while (!stoppingToken.IsCancellationRequested)
-            // {
-            //     try
-            //     {
-            //         var blockIndex = _mySqlStore.GetTip();
-            //         var mocas = _mySqlStore.GetMocas(offset);
-            //         var collectionSheet = _stateContext.WorldState.GetSheet<CollectionSheet>();
-            //         foreach (var moca in mocas)
-            //         {
-            //             var avatars = _mySqlStore.GetAvatarsFromSigner(moca.Signer);
-            //             var migrated = true;
-            //             foreach (var avatar in avatars)
-            //             {
-            //                 try
-            //                 {
-            //                     var collectionState = _stateContext.WorldState.GetCollectionState(new Address(avatar.Address!));
-            //                     var existIds = avatar.ActivateCollections.Select(i => i.Id);
-            //                     var targetIds = collectionState.Ids.Except(existIds);
-            //                     foreach (var collectionId in targetIds)
-            //                     {
-            //                         var row = collectionSheet[collectionId];
-            //                         var options = new List<CollectionOptionModel>();
-            //                         foreach (var modifier in row.StatModifiers)
-            //                         {
-            //                             var option = new CollectionOptionModel
-            //                             {
-            //                                 StatType = modifier.StatType.ToString(),
-            //                                 OperationType = modifier.Operation.ToString(),
-            //                                 Value = modifier.Value,
-            //                             };
-            //                             options.Add(option);
-            //                         }
-            //
-            //                         var collectionModel = new ActivateCollectionModel
-            //                         {
-            //                             ActionId = "Migrate from worker",
-            //                             Avatar = avatar,
-            //                             BlockIndex = blockIndex,
-            //                             CollectionId = collectionId,
-            //                             Options = options,
-            //                         };
-            //                         avatar.ActivateCollections.Add(collectionModel);
-            //                         _mySqlStore.UpdateAvatar(avatar);
-            //                     }
-            //                 }
-            //                 catch (Exception e)
-            //                 {
-            //                     migrated = false;
-            //                     Log.Error(e, "Unexpected exception occurred during MocaWorker: {Exc}", e);
-            //                 }
-            //             }
-            //
-            //             if (migrated)
-            //             {
-            //                 moca.Migrated = true;
-            //                 _mySqlStore.UpdateMoca(moca.Signer);
-            //             }
-            //         }
-            //
-            //         offset += mocas.Count;
-            //         Log.Information("[MocaWorker]{OffSet} migration completed", offset + 100);
-            //     }
-            //     catch (Exception e)
-            //     {
-            //         Log.Error(e, "Unexpected exception occurred during MocaWorker: {Exc}", e);
-            //     }
-            //
-            //     await Task.Delay(100, stoppingToken);
-            // }
-            Log.Information("Start MocaWorker");
-            var avatarAddress = "0x715E63D165236eDF9cD33EAA87d8D21ECFe6eDd9";
-            var agentAddress = "0x0C3109D3a9B93AF4b4538cDA55A5783f0Db68248";
-            var moca = _mySqlStore.GetMoca(agentAddress);
-            Log.Information("[MocaWorker] integration: {Signer}, {Migrated}", moca.Signer, moca.Migrated);
-            var avatar = _mySqlStore.GetAvatar(new Address(avatarAddress), true);
-            Log.Information("[MocaWorker] avatar: {Signer}, {Address}, {Collections}", avatar.AgentAddress, avatar.Address, avatar.ActivateCollections.Count);
-            foreach (var collection in avatar.ActivateCollections)
+            var offset = 0;
+            var previousOffset = 0;
+            while (!stoppingToken.IsCancellationRequested)
             {
-                Log.Information("[MocaWorker] activateCollection: {Address}, {BlockIndex}, {CollectionId}", collection.AvatarAddress, collection.BlockIndex, collection.CollectionId);
-            }
-
-            var collectionSheet = _stateContext.WorldState.GetSheet<CollectionSheet>();
-            var collectionState = _stateContext.WorldState.GetCollectionState(new Address(avatar.Address!));
-            var existIds = avatar.ActivateCollections.Select(i => i.CollectionId);
-            var targetIds = collectionState.Ids.Except(existIds).ToList();
-            var blockIndex = _mySqlStore.GetTip();
-            foreach (var targetId in targetIds)
-            {
-                Log.Information("[MocaWorker] activateCollection new id: {Id}", targetId);
                 try
                 {
-                    foreach (var collectionId in targetIds)
+                    var blockIndex = _mySqlStore.GetTip();
+                    var mocas = _mySqlStore.GetMocas(offset);
+                    var collectionSheet = _stateContext.WorldState.GetSheet<CollectionSheet>();
+                    foreach (var moca in mocas)
                     {
-                        var row = collectionSheet[collectionId];
-                        var options = new List<CollectionOptionModel>();
-                        foreach (var modifier in row.StatModifiers)
+                        var avatars = _mySqlStore.GetAvatarsFromSigner(moca.Signer);
+                        var migrated = true;
+                        foreach (var avatar in avatars)
                         {
-                            var option = new CollectionOptionModel
+                            try
                             {
-                                StatType = modifier.StatType.ToString(),
-                                OperationType = modifier.Operation.ToString(),
-                                Value = modifier.Value,
-                            };
-                            options.Add(option);
+                                var collectionState = _stateContext.WorldState.GetCollectionState(new Address(avatar.Address!));
+                                var existIds = avatar.ActivateCollections.Select(i => i.CollectionId);
+                                var targetIds = collectionState.Ids.Except(existIds);
+                                foreach (var collectionId in targetIds)
+                                {
+                                    var row = collectionSheet[collectionId];
+                                    var options = new List<CollectionOptionModel>();
+                                    foreach (var modifier in row.StatModifiers)
+                                    {
+                                        var option = new CollectionOptionModel
+                                        {
+                                            StatType = modifier.StatType.ToString(),
+                                            OperationType = modifier.Operation.ToString(),
+                                            Value = modifier.Value,
+                                        };
+                                        options.Add(option);
+                                    }
+
+                                    var collectionModel = new ActivateCollectionModel
+                                    {
+                                        ActionId = "Migrate from worker",
+                                        Avatar = avatar,
+                                        BlockIndex = blockIndex,
+                                        CollectionId = collectionId,
+                                        Options = options,
+                                    };
+                                    avatar.ActivateCollections.Add(collectionModel);
+                                }
+
+                                _mySqlStore.UpdateAvatar(avatar);
+                            }
+                            catch (Exception e)
+                            {
+                                migrated = false;
+                                Log.Error(e, "Unexpected exception occurred during MocaWorker: {Exc}", e);
+                            }
                         }
 
-                        var collectionModel = new ActivateCollectionModel
+                        if (migrated)
                         {
-                            ActionId = "Migrate from worker",
-                            Avatar = avatar,
-                            BlockIndex = blockIndex,
-                            CollectionId = collectionId,
-                            Options = options,
-                        };
-                        avatar.ActivateCollections.Add(collectionModel);
+                            moca.Migrated = true;
+                            _mySqlStore.UpdateMoca(moca.Signer);
+                        }
                     }
 
-                    _mySqlStore.UpdateAvatar(avatar);
-                    moca.Migrated = true;
-                    _mySqlStore.UpdateMoca(moca.Signer);
+                    previousOffset = offset;
+                    offset += mocas.Count;
+                    Log.Information("[MocaWorker]{OffSet} migration completed", offset + 100);
                 }
                 catch (Exception e)
                 {
-                    Log.Error(e, "[MocaWorker] Unexpected exception occurred during MocaWorker: {Exc}", e);
+                    Log.Error(e, "Unexpected exception occurred during MocaWorker: {Exc}", e);
                 }
-            }
 
-            await Task.CompletedTask;
+                if (offset == previousOffset)
+                {
+                    break;
+                }
+
+                await Task.Delay(100, stoppingToken);
+            }
         }
     }
 }
