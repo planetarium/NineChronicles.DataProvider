f61de92dccbbd8b0b64b58126a2005b966d95332
Yang Chun Ung <qooraven@gmail.com>
2024-05-30 19:25:47 +0900

    Debug case by env

--- NineChronicles.DataProvider/MocaWorker.cs
+++ NineChronicles.DataProvider/MocaWorker.cs
@@ -27,6 +27,11 @@ namespace NineChronicles.DataProvider
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            await Debug();
+        }
+
+        private async Task Migrate(CancellationToken stoppingToken)
+        {
             var offset = 0;
             var previousOffset = 0;
             while (!stoppingToken.IsCancellationRequested)
@@ -105,6 +110,73 @@ namespace NineChronicles.DataProvider
 
                 await Task.Delay(100, stoppingToken);
             }
+        }
+
+        private async Task Debug()
+        {
+            var avatarAddress = Environment.GetEnvironmentVariable("NC_AvatarAddress")!;
+            var agentAddress = Environment.GetEnvironmentVariable("NC_AgentAddress")!;
+            var moca = _mySqlStore.GetMoca(agentAddress);
+            Log.Information("[MocaWorker] integration: {Signer}, {Migrated}", moca.Signer, moca.Migrated);
+            var avatar = _mySqlStore.GetAvatar(new Address(avatarAddress), true);
+            Log.Information("[MocaWorker] avatar: {Signer}, {Address}, {Collections}", avatar.AgentAddress, avatar.Address, avatar.ActivateCollections.Count);
+            foreach (var collection in avatar.ActivateCollections)
+            {
+                Log.Information("[MocaWorker] activateCollection: {Address}, {BlockIndex}, {CollectionId}", collection.AvatarAddress, collection.BlockIndex, collection.CollectionId);
+            }
+
+            var collectionSheet = _stateContext.WorldState.GetSheet<CollectionSheet>();
+            var collectionState = _stateContext.WorldState.GetCollectionState(new Address(avatar.Address!));
+            var existIds = avatar.ActivateCollections.Select(i => i.Id);
+            var targetIds = collectionState.Ids.Except(existIds).ToList();
+            var blockIndex = _mySqlStore.GetTip();
+            foreach (var targetId in targetIds)
+            {
+                Log.Information("[MocaWorker] activateCollection new id: {Id}", targetId);
+                try
+                {
+                    foreach (var collectionId in targetIds)
+                    {
+                        var row = collectionSheet[collectionId];
+                        var options = new List<CollectionOptionModel>();
+                        foreach (var modifier in row.StatModifiers)
+                        {
+                            var option = new CollectionOptionModel
+                            {
+                                StatType = modifier.StatType.ToString(),
+                                OperationType = modifier.Operation.ToString(),
+                                Value = modifier.Value,
+                            };
+                            options.Add(option);
+                        }
+
+                        var collectionModel = new ActivateCollectionModel
+                        {
+                            ActionId = "Migrate from worker",
+                            Avatar = avatar,
+                            BlockIndex = blockIndex,
+                            CollectionId = collectionId,
+                            Options = options,
+                        };
+                        avatar.ActivateCollections.Add(collectionModel);
+                    }
+
+                    // 업데이트 대상이 있을때만 UPDATE 처리
+                    if (targetIds.Any())
+                    {
+                        _mySqlStore.UpdateAvatar(avatar);
+                    }
+
+                    moca.Migrated = true;
+                    _mySqlStore.UpdateMoca(moca.Signer);
+                }
+                catch (Exception e)
+                {
+                    Log.Error(e, "[MocaWorker] Unexpected exception occurred during MocaWorker: {Exc}", e);
+                }
+            }
+
+            await Task.CompletedTask;
         }
     }
 }
