6b8fa2f8305f265885d62a44ea0947d34b004111
area363 <kidonseo@gmail.com>
2024-07-10 15:14:09 +0900

    test

--- NineChronicles.DataProvider/Store/MySql/AdventureBossStore.cs
+++ NineChronicles.DataProvider/Store/MySql/AdventureBossStore.cs
@@ -14,7 +14,7 @@ namespace NineChronicles.DataProvider.Store
     {
         public async partial Task StoreAdventureBossSeasonList(List<AdventureBossSeasonModel> seasonList)
         {
-            Log.Debug($"[Adventure Boss] StoreAdventureBossSeason: {seasonList.Count}");
+            Console.WriteLine($"[Adventure Boss] StoreAdventureBossSeason: {seasonList.Count}");
             NineChroniclesContext? ctx = null;
             try
             {
@@ -26,12 +26,12 @@ namespace NineChronicles.DataProvider.Store
                     var existSeason = await ctx.AdventureBossSeason.FirstOrDefaultAsync(s => s.Season == season.Season);
                     if (existSeason is null)
                     {
-                        Log.Debug("[Adventure Boss] Season not exist.");
+                        Console.WriteLine("[Adventure Boss] Season not exist.");
                         await ctx.AdventureBossSeason.AddAsync(season);
                     }
                     else
                     {
-                        Log.Debug("[Adventure Boss] Season Exist: update");
+                        Console.WriteLine("[Adventure Boss] Season Exist: update");
                         existSeason.RaffleReward = season.RaffleReward;
                         existSeason.RaffleWinnerAddress = season.RaffleWinnerAddress;
                         ctx.AdventureBossSeason.Update(existSeason);
@@ -42,7 +42,7 @@ namespace NineChronicles.DataProvider.Store
             }
             catch (Exception e)
             {
-                Log.Debug(e.Message);
+                Console.WriteLine(e.Message);
             }
             finally
             {
@@ -55,7 +55,7 @@ namespace NineChronicles.DataProvider.Store
 
         public async partial Task StoreAdventureBossWantedList(List<AdventureBossWantedModel> wantedList)
         {
-            Log.Debug($"[Adventure Boss] StoreAdventureBossWantedList: {wantedList.Count}");
+            Console.WriteLine($"[Adventure Boss] StoreAdventureBossWantedList: {wantedList.Count}");
             NineChroniclesContext? ctx = null;
             try
             {
@@ -69,13 +69,13 @@ namespace NineChronicles.DataProvider.Store
                     }
                 }
 
-                Log.Debug("[Adventure Boss] Wanted Added");
+                Console.WriteLine("[Adventure Boss] Wanted Added");
                 await ctx.SaveChangesAsync();
-                Log.Debug("[Adventure Boss] Wanted Saved");
+                Console.WriteLine("[Adventure Boss] Wanted Saved");
             }
             catch (Exception e)
             {
-                Log.Debug(e.Message);
+                Console.WriteLine(e.Message);
             }
             finally
             {
@@ -88,7 +88,7 @@ namespace NineChronicles.DataProvider.Store
 
         public async partial Task StoreAdventureBossChallengeList(List<AdventureBossChallengeModel> challengeList)
         {
-            Log.Debug($"[Adventure Boss] StoreAdventureBossChallenge: {challengeList.Count}");
+            Console.WriteLine($"[Adventure Boss] StoreAdventureBossChallenge: {challengeList.Count}");
             NineChroniclesContext? ctx = null;
             try
             {
@@ -102,13 +102,13 @@ namespace NineChronicles.DataProvider.Store
                     }
                 }
 
-                Log.Debug($"[Adventure Boss] Challenge Added");
+                Console.WriteLine("[Adventure Boss] Challenge Added");
                 await ctx.SaveChangesAsync();
-                Log.Debug("[Adventure Boss] Challenge Saved");
+                Console.WriteLine("[Adventure Boss] Challenge Saved");
             }
             catch (Exception e)
             {
-                Log.Debug(e.Message);
+                Console.WriteLine(e.Message);
             }
             finally
             {
@@ -121,7 +121,7 @@ namespace NineChronicles.DataProvider.Store
 
         public async partial Task StoreAdventureBossRushList(List<AdventureBossRushModel> rushList)
         {
-            Log.Debug($"[Adventure Boss] StoreAdventureBossRush: {rushList.Count}");
+            Console.WriteLine($"[Adventure Boss] StoreAdventureBossRush: {rushList.Count}");
             NineChroniclesContext? ctx = null;
             try
             {
@@ -135,12 +135,13 @@ namespace NineChronicles.DataProvider.Store
                     }
                 }
 
+                Console.WriteLine("[Adventure Boss] Rush Added");
                 await ctx.SaveChangesAsync();
-                Log.Debug("[Adventure Boss] Rush Saved");
+                Console.WriteLine("[Adventure Boss] Rush Saved");
             }
             catch (Exception e)
             {
-                Log.Debug(e.Message);
+                Console.WriteLine(e.Message);
             }
             finally
             {
@@ -153,7 +154,7 @@ namespace NineChronicles.DataProvider.Store
 
         public async partial Task StoreAdventureBossUnlockFloorList(List<AdventureBossUnlockFloorModel> unlockFloorList)
         {
-            Log.Debug($"[Adventure Boss] StoreAdventureBossUnlockFloor: {unlockFloorList.Count}");
+            Console.WriteLine($"[Adventure Boss] StoreAdventureBossUnlockFloor: {unlockFloorList.Count}");
             NineChroniclesContext? ctx = null;
 
             try
@@ -168,12 +169,13 @@ namespace NineChronicles.DataProvider.Store
                     }
                 }
 
+                Console.WriteLine("[Adventure Boss] UnlockFloor Added");
                 await ctx.SaveChangesAsync();
-                Log.Debug("[Adventure Boss] UnlockFloor Saved");
+                Console.WriteLine("[Adventure Boss] UnlockFloor Saved");
             }
             catch (Exception e)
             {
-                Log.Debug(e.Message);
+                Console.WriteLine(e.Message);
             }
             finally
             {
@@ -186,7 +188,7 @@ namespace NineChronicles.DataProvider.Store
 
         public async partial Task StoreAdventureBossClaimRewardList(List<AdventureBossClaimRewardModel> claimList)
         {
-            Log.Debug($"[Adventure Boss] StoreAdventureBossClaimReward: {claimList.Count}");
+            Console.WriteLine($"[Adventure Boss] StoreAdventureBossClaimReward: {claimList.Count}");
             NineChroniclesContext? ctx = null;
 
             try
@@ -201,12 +203,13 @@ namespace NineChronicles.DataProvider.Store
                     }
                 }
 
+                Console.WriteLine("[Adventure Boss] Claim Added");
                 await ctx.SaveChangesAsync();
-                Log.Debug("[Adventure Boss] Claim Saved");
+                Console.WriteLine("[Adventure Boss] Claim Saved");
             }
             catch (Exception e)
             {
-                Log.Debug(e.Message);
+                Console.WriteLine(e.Message);
             }
             finally
             {
