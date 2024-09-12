7cc658bd6841653ca1913780ef76ba9959c13071
hyeon <hyeon@planetariumhq.com>
2024-09-12 21:52:05 +0900

    Introduce CustomEquipmentCraft model

---
+++ NineChronicles.DataProvider.Executable/Migrations/20240912124503_CustomEquipmentCraft.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NineChronicles.DataProvider.Executable.Migrations
+{
+    public partial class CustomEquipmentCraft : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CustomEquipmentCraft",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    BlockIndex = table.Column<long>(type: "bigint", nullable: false),
+                    AgentAddress = table.Column<string>(type: "varchar(255)", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    AvatarAddress = table.Column<string>(type: "varchar(255)", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    SlotIndex = table.Column<int>(type: "int", nullable: false),
+                    RecipeId = table.Column<int>(type: "int", nullable: false),
+                    Relationship = table.Column<int>(type: "int", nullable: false),
+                    Scroll = table.Column<int>(type: "int", nullable: false),
+                    Circle = table.Column<int>(type: "int", nullable: false),
+                    AdditionalMaterials = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    EquipmentId = table.Column<int>(type: "int", nullable: false),
+                    ElementalType = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    IconId = table.Column<int>(type: "int", nullable: false),
+                    TotalCP = table.Column<long>(type: "bigint", nullable: false),
+                    OptionId = table.Column<int>(type: "int", nullable: false),
+                    CraftWithRandom = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    HasRandomOnlyIcon = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    Date = table.Column<DateOnly>(type: "date", nullable: false),
+                    TimeStamp = table.Column<DateTimeOffset>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomEquipmentCraft", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CustomEquipmentCraft_Agents_AgentAddress",
+                        column: x => x.AgentAddress,
+                        principalTable: "Agents",
+                        principalColumn: "Address");
+                    table.ForeignKey(
+                        name: "FK_CustomEquipmentCraft_Avatars_AvatarAddress",
+                        column: x => x.AvatarAddress,
+                        principalTable: "Avatars",
+                        principalColumn: "Address");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomEquipmentCraft_AgentAddress",
+                table: "CustomEquipmentCraft",
+                column: "AgentAddress");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomEquipmentCraft_AvatarAddress",
+                table: "CustomEquipmentCraft",
+                column: "AvatarAddress");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CustomEquipmentCraft");
+        }
+    }
+}
---
+++ NineChronicles.DataProvider/Store/Models/Crafting/CustomEquipmentCraftModel.cs
@@ -0,0 +1,55 @@
+namespace NineChronicles.DataProvider.Store.Models.Crafting
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class CustomEquipmentCraftModel
+    {
+        [Key]
+        public string? Id { get; set; }
+
+        // Common
+        public long BlockIndex { get; set; }
+
+        public string? AgentAddress { get; set; }
+
+        public AgentModel? Agent { get; set; }
+
+        public string? AvatarAddress { get; set; }
+
+        public AvatarModel? Avatar { get; set; }
+
+        // Input
+        public int SlotIndex { get; set; }
+
+        public int RecipeId { get; set; }
+
+        public int Relationship { get; set; }
+
+        public int Scroll { get; set; }
+
+        public int Circle { get; set; }
+
+        public string? AdditionalMaterials { get; set; }
+
+        // Result
+        public int EquipmentId { get; set; }
+
+        public string? ElementalType { get; set; }
+
+        public int IconId { get; set; }
+
+        public long TotalCP { get; set; }
+
+        public int OptionId { get; set; }
+
+        public bool CraftWithRandom { get; set; }
+
+        public bool HasRandomOnlyIcon { get; set; }
+
+        // Time
+        public DateOnly Date { get; set; }
+
+        public DateTimeOffset TimeStamp { get; set; }
+    }
+}
